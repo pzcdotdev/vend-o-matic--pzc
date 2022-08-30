81a7daeb7219f22da1c4cf7495724d9b2a21a316
Paul Zimmerman-Clayton <paul@pzc.dev>
2022-08-30 15:15:01 -0500

    put and delete done

--- vend_o_matic/api/views.py
+++ vend_o_matic/api/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,20 +6,33 @@ from api.models import CoinModel
 
 class CoinViewSet(APIView):
 
+    def get_current_coins(self):
+        return CoinModel.objects.all().count()
+
+    def get_headers(self):
+        return {
+            "X-Coins": f'{self.get_current_coins()}'
+        }
+
     def put(self, request):
+        if not request.data['coin'] == 1:
+            return Response(data={'error': 'Please insert a single coin.'}, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = CoinSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT, 
+            headers=self.get_headers())
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         try:
             coin = CoinModel.objects.latest('id')
         except CoinModel.DoesNotExist:
-            raise Http404
+            return Response(data={'error': 'No coins in machine.'}, status=status.HTTP_404_NOT_FOUND)
 
         coin.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT, 
+                        headers=self.get_headers())
 
         
