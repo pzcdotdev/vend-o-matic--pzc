10d90c3c9c2276d620d4029d095de905c6a3a8ca
Paul Zimmerman-Clayton <paul@pzc.dev>
2022-08-30 10:21:30 -0500

    rest framework installed and api app added

--- vend_o_matic/vend_o_matic/settings.py
+++ vend_o_matic/vend_o_matic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
 
 ROOT_URLCONF = "vend_o_matic.urls"
 
